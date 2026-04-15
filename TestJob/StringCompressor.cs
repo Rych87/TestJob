3b1a6c7825eb146af08a4acc6313b5c54c34456b
rychh <rych2@mail.ru>
2026-04-15 20:09:11 +0300

    Добавьте файлы проекта.

---
+++ TestJob/StringCompressor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJob
+{
+    public static class StringCompressor
+    {
+        public static string CompressString(string inputString)
+        {
+            var charArray = inputString.ToCharArray();
+            if (!charArray.All(c => char.IsLower(c))) throw new ArgumentException("по условию все символы в нижнем регистре");
+
+            StringBuilder sb = new StringBuilder();
+
+            char? prevChar = null;
+            int charOccurency = 0;
+            for (int i = 0; i < charArray.Length; i++) 
+            {
+                char currChar = charArray[i];
+                if(currChar == prevChar)
+                {
+                    charOccurency++;
+                }
+                else
+                {
+                    sb.Append(prevChar);
+                    if (charOccurency > 1)
+                        sb.Append(charOccurency);
+                    charOccurency = 1;
+                }
+                prevChar = currChar;
+            }
+            sb.Append(prevChar);
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using TestJob;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class TestClass
+    {
+        [TestMethod]
+        public void TestStringCompressor()
+        {
+            Assert.AreEqual("a3b2c3d2e", StringCompressor.CompressString("aaabbcccdde"));
+        }
+    }
+}
