c8d77c816b8805c4e6fcb50d653ffbf9fed6a14c
rychh <rych2@mail.ru>
2026-04-16 11:00:30 +0300

    Задание 2

---
+++ TestJob/StaticServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJob
+{
+    public static class StaticServer
+    {
+        private static ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+        private static int counter = 0;
+        public static int Read()
+        {
+            try
+            {
+                locker.EnterReadLock();
+                return counter;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+        public static void AddToCounter(int i)
+        {
+            try
+            {
+                locker.EnterWriteLock();
+                counter += i;
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+    }
+}
